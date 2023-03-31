06f0b2132a48e6a33c6dee551ab0fa1a33638f7a
lucianogarciaz <luciano.garcia@typeform.com>
2023-03-31 15:10:24 -0300

    lint

--- obs/metrics.go
+++ obs/metrics.go
@@ -4,20 +4,20 @@ import "context"
 
 var _ Metrics = NoopMetrics{}
 
-// NoopMetrics is a metrics implementation that does nothing. It's for testing purposes
+// NoopMetrics is a metrics implementation that does nothing. It's for testing purposes.
 type NoopMetrics struct{}
 
-// Count does nothing and returns always nil
+// Count does nothing and returns always nil.
 func (NoopMetrics) Count(context.Context, string, float64, ...Tag) error {
 	return nil
 }
 
-// Gauge does nothing and returns always nil
+// Gauge does nothing and returns always nil.
 func (NoopMetrics) Gauge(context.Context, string, float64, ...Tag) error {
 	return nil
 }
 
-// Histogram does nothing and returns always nil
+// Histogram does nothing and returns always nil.
 func (NoopMetrics) Histogram(context.Context, string, float64, ...Tag) error {
 	return nil
 }
