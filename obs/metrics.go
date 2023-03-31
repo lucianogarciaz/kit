34674c65dea1fe80facdea6191d0a90a059a7569
lucianogarciaz <luciano.garcia@typeform.com>
2023-03-31 15:04:52 -0300

    add metrics

---
+++ obs/metrics.go
@@ -0,0 +1,23 @@
+package obs
+
+import "context"
+
+var _ Metrics = NoopMetrics{}
+
+// NoopMetrics is a metrics implementation that does nothing. It's for testing purposes
+type NoopMetrics struct{}
+
+// Count does nothing and returns always nil
+func (NoopMetrics) Count(context.Context, string, float64, ...Tag) error {
+	return nil
+}
+
+// Gauge does nothing and returns always nil
+func (NoopMetrics) Gauge(context.Context, string, float64, ...Tag) error {
+	return nil
+}
+
+// Histogram does nothing and returns always nil
+func (NoopMetrics) Histogram(context.Context, string, float64, ...Tag) error {
+	return nil
+}
